fa245ddb2e0eaf9af788ea8becc36880ea3986de
acedmicabhishek <academicabhishek@gmail.com>
2025-07-22 18:17:29 +0530

    minor issue

--- BDkernel.c
+++ BDkernel.c
@@ -4,6 +4,6 @@ void kernel_main() {
     print(">> BrainDance OS - A CyberNet Project", 0x0D); // Bright magenta
     print(">> Version: 0.1", 0x0E);                      // Bright yellow
     print(">> Kernel Booted Successfully", 0x0B);        // Bright cyan
-
+    
     while (1) {}
 }
--- lib/memcore.h
+++ lib/memcore.h
@@ -6,5 +6,8 @@ void* memset(void* dest, int value, unsigned int count);
 int strlen(const char* str);
 
 void print(const char* msg, unsigned char color);
+void clear_screen(unsigned char color);
+void print_int(int number, unsigned char color);
+void print_hex(unsigned int number, unsigned char color);
 
 #endif
