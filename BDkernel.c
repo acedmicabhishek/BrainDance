4a2b077e36b1fbba5eebdf86b4d87cb48f627835
acedmicabhishek <acedmicabhishek@gmail.com>
2025-07-26 17:43:48 +0530

    added panic log fmt

--- BDkernel.c
+++ BDkernel.c
@@ -1,9 +1,25 @@
-#include "./lib/memcore.h"
+extern char _bss_start;
+extern char _bss_end;
+extern char _stack_start;
+extern char _stack_end;
 
 void kernel_main() {
-    print(">> BrainDance OS - A CyberNet Project\n", 0x0D); // Bright magenta
-    print(">> Version: 0.1\n", 0x0E);                      // Bright yellow
-    print(">> Kernel Booted Successfully\n", 0x0B);        // Bright cyan
-    
-    while (1) {}
+    clear_screen(0x07);
+
+    print("BrainDance OS Booted Successfully\n\n", 0x0A);
+
+    log("INFO", "System initialized");
+    print("Memory Info:\n", 0x07);
+
+    print("  BSS Size: ", 0x07);
+    print_int(&_bss_end - &_bss_start, 0x07);
+    print(" bytes\n", 0x07);
+
+    print("  Stack Size: ", 0x07);
+    print_int(&_stack_end - &_stack_start, 0x07);
+    print(" bytes\n", 0x07);
+
+    kprintf("Hello %s!\n", "Abheesh");
+    kprintf("Value: %d, Address: %x\n", 1337, 0xB8000);
+
 }
--- lib/memcore.h
+++ lib/memcore.h
@@ -9,5 +9,12 @@ void print(const char* msg, unsigned char color);
 void clear_screen(unsigned char color);
 void print_int(int number, unsigned char color);
 void print_hex(unsigned int number, unsigned char color);
+void print_char(char c, unsigned char color);
+void println(const char* msg, unsigned char color);
+void kprintf(const char* fmt, ...);
 
+
+void panic(const char* msg);
+void log(const char* tag, const char* msg);
+void scroll_up();
 #endif
